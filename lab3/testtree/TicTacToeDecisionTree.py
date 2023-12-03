aa605d42b736b34731935ccf4074d78d5c7a0728
kkolodziejski <kolodziejskikrzysztofhubert@gmail.com>
2023-12-03 11:52:09 +0100

    Lab3 init

---
+++ lab3/testtree/TicTacToeDecisionTree.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import graphviz
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.metrics import confusion_matrix
+
+df = pd.read_csv("ttt.csv")
+(train_set, test_set) = train_test_split(df.values, train_size=0.66,
+                                         random_state=45)
+
+X = train_set[:, [0, 1, 2, 3, 4, 5, 6, 7, 8]]
+y = train_set[:, [9]]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, y)
+dot_data = tree.export_graphviz(clf, out_file=None)
+
+tree.plot_tree(clf)
+
+graph = graphviz.Source(dot_data)
+graph.render("tictactoe")
+
+Xtest = test_set[:, [0, 1, 2, 3, 4, 5, 6, 7, 8]]
+Ytest = test_set[:, [9]]
+print(clf.score(Xtest, Ytest))
+
+predictions = clf.predict(Xtest)
+
+print(confusion_matrix(test_set[:, [9]], predictions))
