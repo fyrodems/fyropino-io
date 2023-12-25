18c74e055c8e1432b721c05f28936e82c2f28cd2
fyrodems <fyrodems@gmail.com>
2023-12-25 23:38:00 +0100

    creditcard - dodanie rysowania drzewa

--- lab3/creditcard/CreditCardFraudDetection.py
+++ lab3/creditcard/CreditCardFraudDetection.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import accuracy_score
 
 # Wczytaj dane z pliku CSV
 data = pd.read_csv('CreditCardFraudDetection.csv')
-# data = pd.read_csv('creditcard_2023.csv')
 
 # Podziel dane na atrybuty (X) i etykiety (y)
 X = data.drop('Class', axis=1)
@@ -17,6 +16,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # Inicjalizuj drzewo decyzyjne
 max_depth_values = range(1, 21)  # Zakres głębokości drzewa do przetestowania
+best_depth = 0
+best_accuracy = 0
 accuracies = []
 
 for max_depth in max_depth_values:
@@ -31,12 +32,28 @@ for max_depth in max_depth_values:
     accuracy = accuracy_score(y_test, y_pred)
     accuracies.append(accuracy)
 
+    # Aktualizuj najlepszą głębokość i dokładność
+    if accuracy > best_accuracy:
+        best_accuracy = accuracy
+        best_depth = max_depth
+
     # Wydrukuj wyniki dla każdej głębokości
     print(f'Max Depth: {max_depth}, Accuracy: {accuracy}')
+
+# Wypisz najlepszą głębokość i dokładność po zakończeniu pętli
+print(f'\nBest Depth: {best_depth}, Best Accuracy: {best_accuracy}')
 
 # Rysuj wykres głębokości drzewa vs. dokładność
 plt.plot(max_depth_values, accuracies, marker='o')
 plt.title('Depth vs. Accuracy')
 plt.xlabel('Max Depth')
 plt.ylabel('Accuracy')
+plt.show()
+
+# Rysuj drzewo decyzyjne o najlepszej głębokości
+best_clf = DecisionTreeClassifier(random_state=100, max_depth=best_depth)
+best_clf.fit(X_train, y_train)
+plt.figure(figsize=(25, 15))
+plot_tree(best_clf, filled=True, feature_names=X.columns, class_names=['0', '1'])
+plt.title(f'Decision Tree (Max Depth = {best_depth})')
 plt.show()
